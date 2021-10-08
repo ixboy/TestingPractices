4434867acb8de42217fb26597bfd156c23a7391c
ixboy <ismaelixboy@gmail.com>
2021-10-08 23:48:21 +0100

    add calculator.multiply method tests

--- task_3/calculator.test.js
+++ task_3/calculator.test.js
@@ -31,3 +31,17 @@ describe('Tests for Calculator subtract method', () => {
     });
 });
 
+describe('Tests for Calculator multiply method', () => {
+
+    test('checks if 1 * 2 = 2',() => {
+        expect(calculator.multiply(1, 2)).toBe(2);
+    });
+
+    test('checks if 5 * 7 = 35',() => {
+        expect(calculator.multiply(5, 7)).toBe(35);
+    });
+
+    test('checks if 130 * 267 = 34710',() => {
+        expect(calculator.multiply(130, 267)).toBe(34710);
+    });
+});
